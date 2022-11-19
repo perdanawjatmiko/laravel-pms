3539405e53e9ac275f7021552723139e3fdd00b6
Perdana Erda <perdanawjatmiko@gmail.com>
2022-11-19 13:12:13 +0700

    update footer

--- resources/views/layouts/partial/footer.blade.php
+++ resources/views/layouts/partial/footer.blade.php
@@ -2,12 +2,12 @@
     <div class="content py-0">
         <div class="row fs-sm">
             <div class="col-sm-6 order-sm-2 mb-1 mb-sm-0 text-center text-sm-end">
-                Crafted with <i class="fa fa-heart text-danger"></i> by <a class="fw-semibold"
-                    href="https://1.envato.market/ydb" target="_blank">pixelcave</a>
+                
             </div>
             <div class="col-sm-6 order-sm-1 text-center text-sm-start">
-                <a class="fw-semibold" href="https://1.envato.market/r6y" target="_blank">Dashmix 5.4</a> &copy;
-                <span data-toggle="year-copy"></span>
+                Crafted by <a class="fw-semibold"
+                    href="https://www.instagram.com/perdanawjatmiko/" target="_blank">Perdana Erda </a> using <a class="fw-semibold" href="https://1.envato.market/r6y" target="_blank">Dashmix 5.4 </a> Theme &copy;
+                    <span data-toggle="year-copy"></span>
             </div>
         </div>
     </div>
